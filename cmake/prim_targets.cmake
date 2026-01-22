25290fcab77ef7aaa2c5edfa261f7328c413478f
EmirShahfie <emirshahfie@gmail.com>
2026-01-22 15:25:03 +0100

    Update 22/1/2026

--- cmake/prim_targets.cmake
+++
@@ -1,7 +0,0 @@
-add_executable(bradv1 ${PRIM_DIR}/demo-attacks/bradv1.c)
-add_executable(smart-lock ${PRIM_DIR}/demo-attacks/smart-lock.c)
-add_executable(bradv2 ${PRIM_DIR}/demo-attacks/bradv2.c)
-
-add_dump_target(bradv1)
-add_dump_target(smart-lock)
-add_dump_target(bradv2)
